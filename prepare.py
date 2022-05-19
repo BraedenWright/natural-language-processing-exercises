f341419ec410688dcdfc22a6853dec73e40ad19a
Braeden Wright <braeden.wright21@gmail.com>
2022-05-19 09:29:57 -0500

    main

---
+++ prepare.py
@@ -0,0 +1,18 @@
+# Imports
+
+# Basics
+import numpy as np
+import pandas as pd
+import re
+import requests
+
+# 
+import unicodedata
+import json
+from time import strftime
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+
+# Custom
+import acquire
